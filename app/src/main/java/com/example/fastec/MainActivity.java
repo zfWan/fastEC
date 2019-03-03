467b874dac50de8d578f239354e760ae5170dd39
zfWan <43598129+zfWan@users.noreply.github.com>
2019-03-03 12:54:30 +0800

    init

--- app/src/main/java/com/example/fastec/MainActivity.java
+++ app/src/main/java/com/example/fastec/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//加一条注释试试
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
